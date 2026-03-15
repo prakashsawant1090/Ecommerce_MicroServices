c73b170e5fdd340fd629315d4316e293ee91b77c
Prakash Sawant <prakashsawant1090@gmail.com>
2026-03-15 12:00:41 +0530

    Added Micrometer tracing and Zipkin integration for distributed request tracing(to inderstand which subsequent call taking how much time with zipiker dashboard)

--- Api-Gateway/src/main/java/com/prakash/ecommerce/Api_Gateway/Config/AppConfig.java
+++ Api-Gateway/src/main/java/com/prakash/ecommerce/Api_Gateway/Config/AppConfig.java
@@ -1,25 +1,13 @@
-package com.prakash.ecommerce.inventory_service.Config;
+package com.prakash.ecommerce.Api_Gateway.Config;
 
 import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 
 @Configuration
-public class Appconfig {
-
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
-
-    @Bean
-    public RestClient restClient(){
-        return RestClient.builder().build();
-    }
+public class AppConfig {
 
     @Bean
     public Capability micrometerCapability(final MeterRegistry meterRegistry) {
